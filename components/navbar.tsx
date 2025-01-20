2d356323e7d903115c7ebf1487de49557e43afec
Michael Hayk <59941266+mn-cs@users.noreply.github.com>
2025-01-19 17:57:23 -0800

    link to navlink

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -78,15 +78,17 @@ export const Navbar = () => {
         <div className="mx-4 mt-2 flex flex-col gap-2">
           {siteConfig.navMenuItems.map((item, index) => (
             <NavbarMenuItem key={`${item}-${index}`}>
-              <Link
+              <NextLink
                 aria-label={`Navigate to ${item.label}`}
-                className="text-inherit"
+                className={clsx(
+                  linkStyles({ color: "foreground" }),
+                  "data-[active=true]:text-primary data-[active=true]:font-medium"
+                )}
                 href={item.href}
-                size="lg"
-                onPress={handleMenuClose}
+                onClick={handleMenuClose}
               >
                 {item.label}
-              </Link>
+              </NextLink>
             </NavbarMenuItem>
           ))}
         </div>
