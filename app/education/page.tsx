90938317ca4ca85e161ab247ee0231fce780a546
Michael Hayk <59941266+mn-cs@users.noreply.github.com>
2025-02-10 13:21:03 -0800

    adding bullets

--- app/education/page.tsx
+++ app/education/page.tsx
@@ -5,97 +5,120 @@ import { CardHeader, CardBody, CardFooter, Divider } from "@nextui-org/react";
 
 export default function DocsPage() {
   return (
-    <div className="w-full mr-5  ">
-      <Card className="">
-        <CardHeader className="mx-5">
+    <div className="w-full mr-5">
+      <Card className="w-full max-w-lg mx-auto shadow-md mb-5">
+        {/* University Name */}
+        <CardHeader className="mx-5 text-lg font-semibold">
           University of California San Diego
         </CardHeader>
+
         <Divider className="w-[95%] mx-auto" />
-        <CardBody className="ml-5">
-          <p>Master&apos;s Degree in Data Science</p>
-          <p className="text-small mr-5 text-default-500">
-            April 2024 - June 2026
+
+        {/* Degree & Dates */}
+        <CardBody className="ml-5 space-y-1">
+          <p className="text-medium font-medium">
+            Master&apos;s Degree in Data Science
           </p>
+          <p className="text-small text-default-500">April 2024 - June 2026</p>
         </CardBody>
+
         <Divider className="w-[95%] mx-auto" />
-        <CardBody className="ml-5">
-          <p className="">Coursework</p>
-          <p className="text-small mr-5 text-default-500">
-            DSC 255R Machine Learning Fundamentals
-          </p>
-          <p className="text-small mr-5 text-default-500">
-            DSC 207R Python for Data Science
-          </p>
-          <p className="text-small mr-5 text-default-500">
-            DSC 215R Foundations of Probability and Statistics in Data Science
-          </p>
+
+        {/* Coursework */}
+        <CardBody className="ml-5 space-y-1">
+          <p className="font-medium">Coursework</p>
+          <ul className="list-disc list-inside text-small text-default-500 space-y-1">
+            <li>DSC 255R Machine Learning Fundamentals</li>
+            <li>DSC 207R Python for Data Science</li>
+            <li>
+              DSC 215R Foundations of Probability and Statistics in Data Science
+            </li>
+          </ul>
         </CardBody>
+
         <Divider className="w-[95%] mx-auto" />
-        <CardFooter className="ml-5">
-          <p>San Diego, California</p>
+
+        {/* Location */}
+        <CardFooter className="ml-5 text-small text-default-700">
+          San Diego, California
         </CardFooter>
       </Card>
-      <Card className="w-full mt-5">
-        <CardHeader className="ml-5">Los Angeles City College </CardHeader>
+      <Card className="w-full max-w-lg mx-auto shadow-md mb-5">
+        {/* College Name */}
+        <CardHeader className="ml-5 text-lg font-semibold">
+          Los Angeles City College
+        </CardHeader>
+
         <Divider className="w-[95%] mx-auto" />
-        <CardBody className="mx-5">
-          <p>Computer Science in Data Science</p>
-          <p className="text-small mr-5 text-default-500">
+
+        {/* Program Name & Dates */}
+        <CardBody className="mx-5 space-y-1">
+          <p className="text-medium font-medium">
+            Computer Science and Data Science
+          </p>
+          <p className="text-small text-default-500">
             February 2022 - June 2024
           </p>
         </CardBody>
+
         <Divider className="w-[95%] mx-auto" />
-        <CardBody className="ml-5">
-          <p className="">Coursework</p>
-          <p className="text-small mr-5 text-default-500">
-            CS 216 Object-Oriented Programming C++
-          </p>
-          <p className="text-small mr-5 text-default-500">
-            MATH 261 Calculus I
-          </p>
-          <p className="text-small mr-5 text-default-500">
-            MATH 229 Statistis for Data Science
-          </p>
-          <p className="text-small mr-5 text-default-500">
-            CS 119 Python Programming
-          </p>
-          <p className="text-small mr-5 text-default-500">
-            CS 116 Programming In C++
-          </p>
-          <p className="text-small mr-5 text-default-500">
-            MATH 260 Precalculus
-          </p>
+
+        {/* Coursework */}
+        <CardBody className="ml-5 space-y-1">
+          <p className="font-medium">Coursework</p>
+          <ul className="list-disc list-inside text-small text-default-500 space-y-1">
+            <li>CS 216 Object-Oriented Programming C++</li>
+            <li>MATH 261 Calculus I</li>
+            <li>MATH 229 Statistics for Data Science</li>
+            <li>CS 119 Python Programming</li>
+            <li>CS 116 Programming in C++</li>
+            <li>MATH 260 Precalculus</li>
+          </ul>
         </CardBody>
+
         <Divider className="w-[95%] mx-auto" />
-        <CardFooter className="ml-5">Los Angeles, California</CardFooter>
+
+        {/* Location */}
+        <CardFooter className="ml-5 text-small text-default-700">
+          Los Angeles, California
+        </CardFooter>
       </Card>
-      <Card className="w-full mt-5">
-        <CardHeader className="ml-5">
-          University of California, Los Angeles{" "}
+      <Card className="w-full max-w-lg mx-auto shadow-md mb-5">
+        {/* University Name */}
+        <CardHeader className="ml-5 text-lg font-semibold">
+          University of California, Los Angeles
         </CardHeader>
+
         <Divider className="w-[95%] mx-auto" />
-        <CardBody className="ml-5">
-          <p>Computer Science Web Coding Boot Camp</p>
-          <p className="text-small mr-5 text-default-500">
-            June 2019 - March 2020
+
+        {/* Program */}
+        <CardBody className="ml-5 space-y-1">
+          <p className="text-medium font-medium">
+            Computer Science Web Coding Boot Camp
           </p>
+          <p className="text-small text-default-500">June 2019 - March 2020</p>
         </CardBody>
+
         <Divider className="w-[95%] mx-auto" />
-        <CardBody className="ml-5">
-          <p className="">Coursework</p>
-          <p className="text-small mr-5 text-default-500">
-            DESMA X 481.61 Frontend Web Coding Bootcamp
-          </p>
-          <p className="text-small mr-5 text-default-500">
-            COM SCI X 414.20 Fundamentals of Software Development
-          </p>
+
+        {/* Coursework */}
+        <CardBody className="ml-5 space-y-1">
+          <p className="font-medium">Coursework</p>
+          <ul className="list-disc list-inside text-small text-default-500 space-y-1">
+            <li>DESMA X 481.61 Frontend Web Coding Bootcamp</li>
+            <li>COM SCI X 414.20 Fundamentals of Software Development</li>
+          </ul>
         </CardBody>
-        <CardFooter className="ml-5">Los Angeles, California</CardFooter>
+
+        <Divider className="w-[95%] mx-auto" />
+
+        {/* Location */}
+        <CardFooter className="ml-5 text-small text-default-700">
+          Los Angeles, California
+        </CardFooter>
       </Card>
-      <Card className="w-full mt-5">
-        <CardHeader className="ml-5">
-          Armenian National Agrarian University{" "}
-        </CardHeader>
+      <Card className="w-full">
+        <CardHeader className="ml-5">Armenian National University </CardHeader>
         <Divider className="w-[95%] mx-auto" />
         <CardBody className="ml-5">
           <p>Bachelor of Engineering </p>
--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -6,34 +6,65 @@ import { CardHeader, CardBody, CardFooter, Divider } from "@nextui-org/react";
 export default function DocsPage() {
   return (
     <div className="w-full mr-5 mt-7 ">
-      <Card className="">
-        <CardHeader className="mx-5">Programming Languages:</CardHeader>
+      <Card className="w-full  mx-auto">
+        {/* Header */}
+        <CardHeader className="ml-5 text-lg">Programming Languages</CardHeader>
         <Divider className="w-[95%] mx-auto" />
-        <CardBody className="ml-5">
-          <p>Python, JavaScript, C++</p>
+        {/* Content */}
+        <CardBody className="ml-5 text-default-500 text-medium">
+          <ul className="list-disc list-inside space-y-1">
+            <li>Python</li>
+            <li>JavaScript</li>
+            <li>C++</li>
+          </ul>
         </CardBody>
       </Card>
-      <Card className="w-full mt-5">
-        <CardHeader className="ml-5">Libraries: </CardHeader>
+      <Card className="w-full mx-auto mt-5">
+        {/* Header */}
+        <CardHeader className="ml-5 text-lg">Libraries</CardHeader>
+
         <Divider className="w-[95%] mx-auto" />
-        <CardBody className="mx-5">
-          <p className=" mr-5">
-            NumPy, SciPy, Pandas, Matplotlib, Seaborn, Scikit-learn, NLTK, Karas
-          </p>
+
+        {/* Content */}
+        <CardBody className="ml-5 text-default-500 text-medium">
+          <ul className="list-disc list-inside space-y-1">
+            <li>NumPy</li>
+            <li>SciPy</li>
+            <li>Pandas</li>
+            <li>Matplotlib</li>
+            <li>Seaborn</li>
+            <li>Scikit-learn</li>
+          </ul>
         </CardBody>
       </Card>
-      <Card className="w-full mt-5">
-        <CardHeader className="ml-5">Tools/Platforms: </CardHeader>
+      <Card className="w-full mx-auto shadow-md mt-5">
+        {/* Header */}
+        <CardHeader className="ml-5 text-lg">Tools & Platforms</CardHeader>
+
         <Divider className="w-[95%] mx-auto" />
-        <CardBody className="ml-5">
-          <p className=" mr-5">Jupyter Notebook, JupyterLab, Colab, AWS</p>
+
+        {/* Content */}
+        <CardBody className="ml-5 text-default-500 text-medium">
+          <ul className="list-disc list-inside space-y-1">
+            <li>Jupyter Notebook</li>
+            <li>JupyterLab</li>
+            <li>Google Colab</li>
+            <li>AWS</li>
+          </ul>
         </CardBody>
       </Card>
-      <Card className="w-full mt-5">
-        <CardHeader className="ml-5">Databases:</CardHeader>
+      <Card className="w-full mx-auto shadow-md mt-5">
+        {/* Header */}
+        <CardHeader className="ml-5 text-lg ">Databases</CardHeader>
+
         <Divider className="w-[95%] mx-auto" />
-        <CardBody className="ml-5">
-          <p>SQL, MongoDB</p>
+
+        {/* Content */}
+        <CardBody className="ml-5 text-default-500 text-medium">
+          <ul className="list-disc list-inside space-y-1">
+            <li>SQL</li>
+            <li>MongoDB</li>
+          </ul>
         </CardBody>
       </Card>
     </div>
