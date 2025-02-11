047d1fddeb17fdfdb13c338e15bc2d2fd265c0b3
Michael Hayk <59941266+mn-cs@users.noreply.github.com>
2025-02-10 18:21:52 -0800

    +t

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Michael Bennet",
+  name: "Michael Hayk",
   description: "Data Scientist.",
   navItems: [
     {
