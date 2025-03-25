da6c7a32229b17e6ab9cf92eb613186ab150b703
Michael Hayk <59941266+mn-cs@users.noreply.github.com>
2025-03-25 11:06:06 -0700

    [GA4] Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,6 +37,19 @@ export default function RootLayout({
   return (
     <html suppressHydrationWarning lang="en">
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-T29GPCFH');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
         <meta
           name="google-site-verification"
           content="Ju9v4ymkCxJzPMLmV7iGjxqPNLUjMthdP4UGCSBFN-c"
@@ -72,6 +85,16 @@ export default function RootLayout({
           fontSans.variable
         )}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `
+              <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T29GPCFH"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>
+            `,
+          }}
+        />
+        {/* End Google Tag Manager (noscript) */}
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
           <div className="relative flex flex-col h-screen max-w-4xl mx-auto ">
             <Navbar />
